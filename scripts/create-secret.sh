299b0c069dd7fa2ca72a5a14e0585a38dd178ea9
Airowire Docs Server <airowire@docs>
2026-06-11 13:24:52 +0000

    Update Datadog onboarding automation

--- scripts/create-secret.sh
+++ scripts/create-secret.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+set -e
+
 DD_API_KEY=$(aws secretsmanager get-secret-value \
+  --region ap-south-2 \
   --secret-id datadog/api-key \
   --query SecretString \
   --output text)
 
+CLUSTER_AGENT_TOKEN=$(aws secretsmanager get-secret-value \
+  --region ap-south-2 \
+  --secret-id datadog/cluster-agent-token \
+  --query SecretString \
+  --output text)
+
 kubectl create namespace datadog-agent \
---dry-run=client -o yaml | kubectl apply -f -
+  --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret generic datadog-secret \
--n datadog-agent \
---from-literal api-key="$DD_API_KEY" \
---from-literal token="$(openssl rand -hex 16)" \
---dry-run=client -o yaml | kubectl apply -f -
+  -n datadog-agent \
+  --from-literal api-key="$DD_API_KEY" \
+  --from-literal token="$CLUSTER_AGENT_TOKEN" \
+  --dry-run=client -o yaml | kubectl apply -f -
