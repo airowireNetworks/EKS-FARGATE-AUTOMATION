f707f55a20bf6bf88a74cf6cb1fc22ddf682ab5e
mohammedsaqlain0230 <mohammed@airowire.com>
2026-06-12 02:06:42 +0530

    Update create-secret.sh

--- scripts/create-secret.sh
+++ scripts/create-secret.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
+
 set -e
+
+echo "Fetching Datadog API Key from Secrets Manager..."
+
 DD_API_KEY=$(aws secretsmanager get-secret-value \
- --region ap-south-2 \
- --secret-id datadog/api-key \
- --query SecretString \
- --output text)
+  --region ap-south-2 \
+  --secret-id datadog/api-key \
+  --query SecretString \
+  --output text | jq -r '."datadog-api-key"')
+
 CLUSTER_AGENT_TOKEN=$(aws secretsmanager get-secret-value \
- --region us-east-1 \
- --secret-id datadog/cluster-agent-token \
- --query SecretString \
- --output text)
+  --region us-east-1 \
+  --secret-id datadog/cluster-agent-token \
+  --query SecretString \
+  --output text)
+
+echo "Creating namespace..."
 kubectl create namespace datadog-agent \
- --dry-run=client -o yaml | kubectl apply -f -
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Creating Datadog secret..."
 kubectl create secret generic datadog-secret \
- -n datadog-agent \
- --from-literal api-key="$DD_API_KEY" \
- --from-literal token="$CLUSTER_AGENT_TOKEN" \
- --dry-run=client -o yaml | kubectl apply -f -
+  -n datadog-agent \
+  --from-literal=api-key="$DD_API_KEY" \
+  --from-literal=token="$CLUSTER_AGENT_TOKEN" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Datadog secret created successfully."
