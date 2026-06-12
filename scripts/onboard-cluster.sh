ca7a1f43ba103cd947868758dc6abb69902eae14
mohammedsaqlain0230 <mohammed@airowire.com>
2026-06-12 16:15:56 +0530

    Update onboard-cluster.sh

--- scripts/onboard-cluster.sh
+++ scripts/onboard-cluster.sh
@@ -5,8 +5,11 @@ set -e
 echo "Creating Datadog Secret..."
 ./scripts/create-secret.sh
 
-echo "Applying RBAC..."
+echo "Applying Fargate RBAC..."
 kubectl apply -f kubernetes/fargate-rbac.yaml
+
+echo "Creating Datadog Workload RBAC..."
+./scripts/create-fargate-workload-rbac.sh
 
 echo "Installing Datadog..."
 ./scripts/install-datadog.sh
@@ -20,4 +23,4 @@ echo "Restarting Deployments..."
 echo "Running Validation..."
 ./scripts/validate.sh
 
-echo "Datadog Onboarding Completed"
+echo "Datadog Onboarding Completed Successfully"
